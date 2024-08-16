1d2453fc35814b5c0bbcea128101dbc08fc3698f
gustavoiinagata <135177885+gustavoiinagata@users.noreply.github.com>
2024-08-16 11:30:55 -0300

    .

--- script.js
+++ script.js
@@ -4,5 +4,47 @@ const caixaAlternativas = document.querySelector( '.caixa-alternativas');
 const caixaResultado = document.querySelector( '.caixa-resultado');
 const textoResultado = document.querySelector( '.texto-resultado');
 
+const perguntas = [
+    {
+        enunciado: "No Âmbito social, você prefere:",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmacao: "afirmação",
+            },
+            {
+                texto: "alternativa 2",
+                afirmacao: "afirmação",
+            },
+        ]
+    },
+    {
+        enunciado: "No Âmbito ambiental, você prefere:",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmacao: "afirmação",
+            },
+            {
+                texto: "alternativa 2",
+                afirmacao: "afirmação",
+            },
+        ]
+    },
+    {
+        enunciado: "No Âmbito tecnológico, você prefere:",
+        alternativas: [
+            {
+                texto: "alternativa 1",
+                afirmacao: "afirmação",
+            },
+            {
+                texto: "alternativa 2",
+                afirmacao: "afirmação",
+            },
+        ]
+    },
+]
+
 let atual = 0;
 let perguntaAtual;
